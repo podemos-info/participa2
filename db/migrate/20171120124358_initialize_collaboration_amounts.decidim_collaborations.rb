6304d2f687cac80f178708aa573b99b05fbae8b2
David Rodríguez <deivid.rodriguez@riseup.net>
2018-03-07 21:06:37 -0300

    Sync old migrations with the ones in crowdfundings

--- db/migrate/20171120124358_initialize_collaboration_amounts.decidim_collaborations.rb
+++ db/migrate/20171120124358_initialize_collaboration_amounts.decidim_collaborations.rb
@@ -1,7 +1,13 @@
 # This migration comes from decidim_collaborations (originally 20171116140837)
+# frozen_string_literal: true
+
 class InitializeCollaborationAmounts < ActiveRecord::Migration[5.1]
+  class Collaboration < ApplicationRecord
+    self.table_name = :decidim_collaborations_collaborations
+  end
+
   def change
-    Decidim::Collaborations::Collaboration.find_each do |collaboration|
+    Collaboration.find_each do |collaboration|
       collaboration.amounts = Decidim::Collaborations.selectable_amounts
       collaboration.save
     end
