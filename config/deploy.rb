c70a1d1ed38ad17cd240af60cf9ea023104daddb
Leonardo Diez <leiodd@gmail.com>
2018-03-21 15:52:48 +0100

    Added uploads folder to linked folders in deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :repo_url, "git@github.com:podemos-info/participa2.git"
 append :linked_files, ".env"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
