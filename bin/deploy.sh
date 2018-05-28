ea3edde5670e773c4df91dc8c55b8f67ec0d37e8
David Rodríguez <deivid.rodriguez@riseup.net>
2018-05-28 14:48:12 -0300

    Improve script's error control and output

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,5 +4,33 @@ set -eo pipefail
 
 if [ "${CIRCLE_BRANCH}" = "master" ]
 then
-  BRANCH=$CIRCLE_BRANCH bundle exec cap staging deploy | sed "s/$STAGING_SERVER_MASTER_HOST/***/g" | sed "s/$STAGING_SERVER_MASTER_PORT/***/g" | sed "s/$STAGING_SERVER_SLAVE_HOST/***/g" | sed "s/$STAGING_SERVER_SLAVE_PORT/***/g"
+  if [ -z "$STAGING_SERVER_MASTER_HOST" ]
+  then
+    echo "You need to set the STAGING_SERVER_MASTER_HOST environemnt variable"
+    exit 1
+  fi
+
+  if [ -z "$STAGING_SERVER_MASTER_PORT" ]
+  then
+    echo "You need to set the STAGING_SERVER_MASTER_PORT environemnt variable"
+    exit 1
+  fi
+
+  if [ -z "$STAGING_SERVER_SLAVE_HOST" ]
+  then
+    echo "You need to set the STAGING_SERVER_SLAVE_HOST environemnt variable"
+    exit 1
+  fi
+
+  if [ -z "$STAGING_SERVER_SLAVE_PORT" ]
+  then
+    echo "You need to set the STAGING_SERVER_SLAVE_PORT environemnt variable"
+    exit 1
+  fi
+
+  BRANCH=$CIRCLE_BRANCH bundle exec cap staging deploy | \
+    sed "s/$STAGING_SERVER_MASTER_HOST/[master_host]/g" | \
+    sed "s/$STAGING_SERVER_MASTER_PORT/[master_port]/g" | \
+    sed "s/$STAGING_SERVER_SLAVE_HOST/[slave_host]/g" | \
+    sed "s/$STAGING_SERVER_SLAVE_PORT/[slave_port]/g"
 fi
