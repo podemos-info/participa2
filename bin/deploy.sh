b68aeab58ce7d588fedf41996c6a6e87113ae132
David Rodríguez <deivid.rodriguez@riseup.net>
2018-05-28 14:26:19 -0300

    Extract to script for easier testing

---
+++ bin/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -eo pipefail
+
+if [ "${CIRCLE_BRANCH}" = "master" ]
+then
+  BRANCH=$CIRCLE_BRANCH bundle exec cap staging deploy | sed "s/$STAGING_SERVER_MASTER_HOST/***/g" | sed "s/$STAGING_SERVER_MASTER_PORT/***/g" | sed "s/$STAGING_SERVER_SLAVE_HOST/***/g" | sed "s/$STAGING_SERVER_SLAVE_PORT/***/g"
+fi
