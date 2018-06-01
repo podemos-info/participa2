8bca68b19113278e32ac3ea85660c13d4d3c5cb3
David Rodríguez <deivid.rodriguez@riseup.net>
2018-06-01 19:43:19 -0300

    Add a bunch of missing tests

--- decidim-module-census_connector/spec/commands/verifications/census/perform_census_data_step_spec.rb
+++ decidim-module-census_connector/spec/commands/verifications/census/perform_census_data_step_spec.rb
@@ -12,7 +12,11 @@ module Decidim::CensusConnector
     let(:user) { create(:user, organization: organization) }
 
     let!(:authorization) { create(:authorization, name: "census", user: user, metadata: { "person_id" => 1 }) }
-    let!(:scope) { create(:scope, code: "ES", organization: organization, id: 1) }
+
+    let!(:local_scope) { create(:scope, code: scope_code, organization: organization) }
+    let!(:foreign_scope) { create(:scope, organization: organization) }
+
+    let(:scope_code) { "ES" }
 
     let(:person_proxy) { PersonProxy.for(user) }
 
@@ -25,18 +29,22 @@ module Decidim::CensusConnector
     let(:address) { "Rua del Percebe, 1" }
     let(:postal_code) { "08001" }
 
+    let(:address_scope_id) { local_scope.id }
+    let(:document_scope_id) { local_scope.id }
+    let(:scope_id) { local_scope.id }
+
     let(:form) do
       Verifications::Census::DataForm.new(
         first_name: first_name,
         last_name1: last_name1,
         document_type: document_type,
         document_id: document_id,
-        document_scope_id: 1,
+        document_scope_id: document_scope_id,
         born_at: born_at,
         gender: gender,
         address: address,
-        address_scope_id: 1,
-        scope_id: nil,
+        address_scope_id: address_scope_id,
+        scope_id: scope_id,
         postal_code: postal_code
       ).with_context(
         local_scope: local_scope
@@ -198,6 +206,95 @@ module Decidim::CensusConnector
       it "adds the API errors to the form" do
         expect(form.errors.count).to eq(1)
         expect(form.errors[:postal_code]).to eq(["can't be blank"])
+      end
+    end
+
+    context "when address_scope_id not present" do
+      let(:address_scope_id) { nil }
+
+      before do
+        stub_request(:patch, "http://mycensus:3001/api/v1/people/1@census")
+          .with(body: hash_including(address_scope_code: nil))
+          .to_return(status: 422, body: '{"address_scope":[{"error":"blank"}]}')
+
+        subject.call
+      end
+
+      it "adds the API errors to the form" do
+        expect(form.errors.count).to eq(1)
+        expect(form.errors[:address_scope_id]).to eq(["can't be blank"])
+      end
+    end
+
+    context "when document_scope_id not present" do
+      let(:document_scope_id) { nil }
+
+      it "sends the local scope instead" do
+        stub = stub_request(
+          :patch, "http://mycensus:3001/api/v1/people/1@census"
+        ).with(
+          body: hash_including(document_scope_code: local_scope.code)
+        ).to_return(status: 202, body: "{}")
+
+        subject.call
+
+        expect(stub).to have_been_requested
+      end
+    end
+
+    context "when scope_id not present" do
+      let(:scope_id) { nil }
+
+      context "and address_scope_id not present either" do
+        let(:address_scope_id) { nil }
+
+        before do
+          stub_request(:patch, "http://mycensus:3001/api/v1/people/1@census")
+            .with(body: hash_including(address_scope_code: nil))
+            .to_return(status: 422, body: '{"address_scope":[{"error":"blank"}]}')
+
+          subject.call
+        end
+
+        it "adds API errors to the form" do
+          expect(form.errors.count).to eq(1)
+          expect(form.errors[:address_scope_id]).to eq(["can't be blank"])
+        end
+      end
+
+      context "and address_scope_id present" do
+        context "and local" do
+          let(:address_scope_id) { local_scope.id }
+
+          it "sends the address scope" do
+            stub = stub_request(
+              :patch, "http://mycensus:3001/api/v1/people/1@census"
+            ).with(
+              body: hash_including(scope_code: local_scope.code)
+            ).to_return(status: 202, body: "{}")
+
+            subject.call
+
+            expect(stub).to have_been_requested
+          end
+        end
+
+        context "and not local" do
+          let(:address_scope_id) { foreign_scope.id }
+
+          before do
+            stub_request(:patch, "http://mycensus:3001/api/v1/people/1@census")
+              .with(body: hash_including(scope_code: nil))
+              .to_return(status: 422, body: '{"scope":[{"error":"blank"}]}')
+
+            subject.call
+          end
+
+          it "adds the API errors to the form" do
+            expect(form.errors.count).to eq(1)
+            expect(form.errors[:scope_id]).to eq(["can't be blank"])
+          end
+        end
       end
     end
   end
