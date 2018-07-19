2d24b5816c8ee6ed3c5c616df3463b5ce167c4d9
Leonardo Diez <leiodd@gmail.com>
2018-07-19 09:19:03 +0200

    Votings cells added

--- decidim-module-votings/app/cells/decidim/votings/voting_m_cell.rb
+++ decidim-module-votings/app/cells/decidim/votings/voting_m_cell.rb
@@ -8,6 +8,10 @@ module Decidim
     class VotingMCell < Decidim::CardMCell
       include VotingCellsHelper
 
+      def voting
+        model
+      end
+
       private
 
       def title
@@ -16,6 +20,77 @@ module Decidim
 
       def description
         translated_attribute model.description
+      end
+
+      def has_image?
+        true
+      end
+
+      def resource_image_path
+        model.image.url
+      end
+
+      def statuses
+        [:voted, :next_date]
+      end
+
+      def next_date_status
+        render :next_date
+      end
+
+      def voted_status
+        render :voted
+      end
+
+      def vote_permission_descriptions
+        @vote_permission_descriptions ||= vote_action_authorizer_class&.try(:describe_options, vote_permission_options)
+      end
+
+      def current_component
+        model.component
+      end
+
+      def has_voted?
+        @has_voted ||= model.has_voted?(current_user)
+      end
+
+      def next_date
+        if voting.started?
+          model.end_date
+        else
+          model.start_date
+        end
+      end
+
+      def vote_action
+        if !voting.started?
+          :simulate_vote
+        elsif has_voted?
+          :change_vote
+        else
+          :vote
+        end
+      end
+
+      def vote_action_authorizer_class
+        return unless vote_authorization_handler_name
+        @vote_action_authorizer_class ||= Verifications.find_workflow_manifest(vote_authorization_handler_name)&.action_authorizer_class
+      end
+
+      def vote_authorization_handler_name
+        vote_permission&.fetch("authorization_handler_name", nil)
+      end
+
+      def vote_permission_options
+        vote_permission&.fetch("options", {})
+      end
+
+      def vote_permission
+        @vote_permission ||= model.permissions&.fetch("vote", nil) || current_component.permissions&.fetch("vote", nil)
+      end
+
+      def vote_path
+        ::Decidim::EngineRouter.main_proxy(current_component).voting_vote_path(voting.id)
       end
     end
   end
