58d05b5e53bb121aeda2b27879fd7ec295232e2e
David Rodríguez <deivid.rodriguez@riseup.net>
2018-09-21 11:07:27 -0300

    Fix seeds (#89)
    * Fix schema discrepancy
    
    * Fix homepage migration to content blocks in seeds
    
    * Make content blocks migration not load real class
    
    * Make DB port configurable
    
    This helped me trying migrations against postgresql 9.5 without having
    to remove postgresql 10 from my system.

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,13 +37,11 @@ class ParticipaSeeder
       instagram_handler: "ahorapodemos",
       youtube_handler: "CirculosPodemos",
       github_handler: "podemos-info",
-      welcome_text: localize("Bienvenido/a al Portal de Participación de Podemos."),
       description: localize("<strong>Podemos</strong> nace con la voluntad de construir una forma nueva de hacer política,
                           y para ello estamos construyendo una estructura transparente, ciudadana, abierta, democrática
                           y eficaz. Una organización que responda al impulso democratizador de Podemos, en la que discutamos,
                           debatamos y decidamos entre todos y todas."),
       logo: File.new(File.join(@base_path, "assets/images/logo.png")),
-      homepage_image: File.new(File.join(@base_path, "assets/images/homepage_image.jpg")),
       official_img_header: File.new(File.join(@base_path, "assets/images/official-logo-header.png")),
       official_img_footer: File.new(File.join(@base_path, "assets/images/official-logo-footer.png")),
       favicon: File.new(File.join(@base_path, "assets/images/icon.svg")),
@@ -59,6 +57,9 @@ class ParticipaSeeder
     Decidim::Assembly.delete_all
 
     Decidim::Core::Engine.load_seed unless Rails.env.production?
+
+    create_homepage_hero_block
+
     Decidim::System::CreateDefaultPages.call(organization) if new_organization?
     Decidim::CensusConnector::Engine.load_seed
   end
@@ -206,6 +207,25 @@ class ParticipaSeeder
         @new_organization = true
       end
     end
+  end
+
+  def create_homepage_hero_block
+    hero_content_block = Decidim::ContentBlock.find_or_initialize_by(
+      organization: organization,
+      scope: :homepage,
+      manifest_name: :hero
+    )
+
+    homepage_image = File.new(File.join(@base_path, "assets/images/homepage_image.jpg"))
+    hero_content_block.images_container.background_image = homepage_image
+
+    welcome_text = localize("Bienvenido/a al Portal de Participación de Podemos.")
+
+    settings = {}
+    settings = welcome_text.inject(settings) { |acc, (k, v)| acc.update("welcome_text_#{k}" => v) }
+
+    hero_content_block.settings = settings
+    hero_content_block.save!
   end
 
   def new_organization?
