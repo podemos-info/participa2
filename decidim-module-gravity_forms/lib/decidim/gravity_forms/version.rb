a1e060da5c2c3b5497c11b49f0ee8713583093e1
Leonardo Diez <leiodd@gmail.com>
2018-10-18 17:03:55 +0200

    Bump dependencies

--- decidim-module-gravity_forms/lib/decidim/gravity_forms/version.rb
+++ decidim-module-gravity_forms/lib/decidim/gravity_forms/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-gravity_forms version.
   module GravityForms
     def self.version
-      "0.14.1"
+      "0.14.3"
     end
   end
 end
--- decidim-module-votings/lib/decidim/votings/version.rb
+++ decidim-module-votings/lib/decidim/votings/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-votings version.
   module Votings
     def self.version
-      "0.14.1"
+      "0.14.3"
     end
   end
 end
