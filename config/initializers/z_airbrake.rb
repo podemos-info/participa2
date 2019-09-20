79eebfa95c5c94c56634f2b483fd33c021cde56e
Leonardo Diez <leiodd@gmail.com>
2019-09-20 16:31:41 +0200

    Fix airbrake (#109)
    * Update airbrake client version
    
    * Airbrake configuration updated

--- config/initializers/z_airbrake.rb
+++ config/initializers/z_airbrake.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
-require "airbrake"
-
 Airbrake.configure do |config|
   config.environment = Rails.env
   config.ignore_environments = %w(test development)
   config.host = Rails.application.secrets.airbrake[:host]
   config.project_id = Rails.application.secrets.airbrake[:project_id]
   config.project_key = Rails.application.secrets.airbrake[:api_key]
+  config.root_directory = Rails.root
+  config.logger = Rails.logger
+  config.blacklist_keys = Rails.application.config.filter_parameters
 end
